b8c3537f588eac6612548830f989cf9487b85318
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-08-08 10:06:28 +0000

    [Add]3-12

---
+++ app/controllers/bookers_controller.rb
@@ -0,0 +1,7 @@
+class BookersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+end
--- db/migrate/20220807071638_create_bookers.rb
+++ db/migrate/20220807071638_create_bookers.rb
@@ -1,6 +1,9 @@
 class CreateBookers < ActiveRecord::Migration[6.1]
   def change
     create_table :bookers do |t|
+      #本のタイトルを'title', 感想を'body'としてstring型で定義する
+      t.string :title
+      t.string :body
 
       t.timestamps
     end
